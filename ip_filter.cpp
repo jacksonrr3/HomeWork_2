7a8e1753b43c4fd062b18275ff69726048765681
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-10-17 14:49:35 +0300

    Update ip_filter.cpp

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -8,7 +8,6 @@
 #include <tuple>
 #include <algorithm>
 
-
 using ip_addr = std::vector<int>;
 using ip_pool = std::multiset<ip_addr, std::greater<>>;
 // ("",  '.') -> [""]
@@ -44,25 +43,6 @@ void print_ip_addr(const ip_addr& ip) {
 		ip[3] << std::endl;
 }
 
-/*
-bool comp_ip_addr(const ip_addr& a, int n) {
-	return (a[0] == n);
-}
-
-bool comp_ip_addr(const ip_addr& a, int n, int m) {
-	return (a[0] == n && a[1] == m);
-}
-
-template <typename...Args>
-void filter(const ip_pool& pool, Args...args) {
-	for (auto it : pool) {
-		if (comp_ip_addr(it, args...)) {
-			print_ip_addr(it);
-		}
-	}
-}
-*/
-
 template <typename...Args>
 void filter(const ip_pool& pool, Args...args) {
 	ip_addr temp = {args...};
@@ -81,27 +61,16 @@ void filter_any(const ip_pool& pool, int a) {
 	}
 }
 
-
 int main(int argc, char const *argv[])
 {
-	//ââîä äàííûõ èç ôàéëà âî âðåìÿ îòëàäêè
-	std::ifstream test_in;
-	test_in.open("C:/Users/Evgeniy/source/repos/HomeWork1_2/ip_filter_3.tsv");
-	
 		try
 		{
-			ip_pool ip_pool; //ñòðêóòóðà äàííûõ, ñîäåðæàùàÿ ip-àäðåñà â ïîðÿäêå óáûâàíèÿ.
+			ip_pool ip_pool; 
 
-			for (std::string line; std::getline(test_in, line);)  //èçìåíèòü test_in íà std::cin
+			for (std::string line; std::getline(std::cin, line);)  
 			{
 				auto v1 = std::move(split(line, '\t'));
 				auto v2 = std::move(split(v1.at(0), '.'));
-			 
-			//	int o = std::stoi(v2[0]);
-			//	int two = std::stoi(v2[1]);
-			//	int three = std::stoi(v2[2]);
-			//	int four = std::stoi(v2[3]);
-			//ip_addr temp{ (std::stoi(v2[0]), std::stoi(v2[1]), std::stoi(v2[2]), std::stoi(v2[3])) };
 				ip_pool.emplace(ip_addr{ std::stoi(v2[0]), std::stoi(v2[1]), std::stoi(v2[2]), std::stoi(v2[3])});
 			}
 
@@ -117,23 +86,18 @@ int main(int argc, char const *argv[])
 			{
 				print_ip_addr(ip);
 			}
-
 		
 			// TODO filter by first byte and output
 			// ip = filter(1)
 			filter(ip_pool, 1);
-
 			
 			// TODO filter by first and second bytes and output
 			// ip = filter(46, 70)
 			filter(ip_pool, 46, 70);
 			
-
 			// TODO filter by any byte and output
 			// ip = filter_any(46)
 			filter_any(ip_pool, 46);
-
-			
 		}
 		catch (const std::exception &e)
 		{
